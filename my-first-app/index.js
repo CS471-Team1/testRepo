ce7820e9faa1d15cea5bb529561cb74f20b6ca1d
lukeptomey <lukeptomey@u.boisestate.edu>
2021-04-09 14:22:25 -0600

    current master

--- my-first-app/index.js
+++ my-first-app/index.js
@@ -1,6 +1,18 @@
+process.env['NODE_TLS_REJECT_UNAUTHORIZED']=0; //This is just to bypass a database SSL error I was getting
+
 const { Octokit } = require("@octokit/core");
 const fs = require('fs');
 const readline = require('readline');
+const { Client } = require('pg');
+var client = new Client({
+  host: "ec2-54-167-168-52.compute-1.amazonaws.com",
+  port: 5432,
+  database: "d3fdn353g17soe",
+  user: "ptwqfwvglwtqst",
+  password: "de8ec3125d28ff9f35b5c7fe1d4746135f7745284cecbd5040d3c2cd84c0cf7c",
+  ssl: true
+});
+createLabelsTable();
 let line_reader;
 
 /**
@@ -11,6 +23,29 @@ let line_reader;
 module.exports = (app) => {
   // Your code here
   app.log.info("Yay, the app was loaded!");
+
+  //Recognizes conflicting tags
+  app.on("issues.opened", async (context) => {
+    //console.log(context.payload.issue.labels);
+
+    let tagnames = [];
+    let conflict = false;
+
+    for(var i of context.payload.issue.labels) {
+      tagnames.push(i.name);
+    }
+
+    if(tagnames.includes('bug') && tagnames.includes('feature')) {
+      conflict = true;
+    }
+
+    if(conflict === true) {
+      const returnComment = context.issue({
+        body: "There appears to be conflicting labels on this issue. Please review the lables you have applied and confirm that they are correct."
+      });
+      return context.octokit.issues.createComment(returnComment);
+    }
+  });
 
   //tag checking functionality plus tag adding functionality
   app.on("issues.opened", async (context) => {
@@ -105,6 +140,9 @@ module.exports = (app) => {
     }
     
     //labels you wish to add to an untagged issue
+    /**
+     * LABELS ARE ONLY FOR DEFAULT GITHUB REPOSITORIES
+     */
     const bugLabel = context.issue({
       labels: ['bug']
     });
@@ -135,23 +173,30 @@ module.exports = (app) => {
 
     const octo = new Octokit();
     for(const repoObject of context.payload.repositories) {
-      const ret = await octo.request('GET /repos/{owner}/{repo}/labels', {
-        owner: context.payload.installation.account.login,
-        repo: repoObject.name
-      });
-
       var repoID = repoObject.id;
-      var fileName = 'label_cache/' + repoID;
-
-      fs.writeFile(fileName, '', function(err) {
-        if(err) { console.log("Couldn't create label cache"); }
-      });
-
-      ret.data.forEach(label => {
-        fs.appendFile(fileName, label.name + ',', function(err) {
-          if(err) throw err;
-        });
-      });
+      var reponame = repoObject.name;
+      var owner = context.payload.installation.account.login;
+      client.query("Select * from labels where RepoID = " + repoID,
+      async function(err, result) {
+        if(result.rowCount == 0) {
+          console.log("No rows returned, getting all labels");
+          var allLabels = await getAllLabels(owner, reponame);
+          let labelString = "";
+          allLabels.data.forEach(label => {
+            labelString += label.name + ","
+          });
+          client.query("Insert into labels (RepoID, RepoName, Owner, Labels) values (" + BigInt(repoID) + ", '" + reponame + "', '" + owner + "', '" + labelString + "');");
+        }
+        else {
+          var allLabels = await getAllLabels(owner, reponame);
+          let labelString = "";
+          allLabels.data.forEach(label => {
+            labelString += label.name + ","
+          });
+          client.query("update labels set Labels = '" + labelString + "' where RepoID = " + BigInt(repoID) + ";");
+        }
+      }
+    );
     }
   });
 
@@ -181,36 +226,73 @@ module.exports = (app) => {
 
   app.on(['label.created', 'label.deleted', 'label.edited'], async (context) => {
     
-    app.log.info(context);
-    const octo = new Octokit();
-    
-    await new Promise(resolve => {
-      setTimeout(resolve, 15000)
-    });
-
-    const ret = await octo.request('GET /repos/{owner}/{repo}/labels', {
-      owner: context.payload.repository.owner.login,
-      repo: context.payload.repository.name
-    });
-
     var repoID = context.payload.repository.id;
-    var fileName = 'label_cache/' + repoID;
+    var reponame = context.payload.repository.name;
+    var owner = context.payload.repository.owner.login;
 
-    fs.writeFile(fileName, '', function(err) {
-      if(err) { console.log("Couldn't create label cache"); }
-    });
-
-    ret.data.forEach(label => {
-      fs.appendFile(fileName, label.name + ',', function(err) {
-        if(err) throw err;
-      });
-    });
-
+    client.query("Select * from labels where RepoID = " + repoID,
+      async function(err, result) {
+        if(result.rowCount == 0) {
+          console.log("No rows returned, getting all labels");
+          var allLabels = await getAllLabels(owner, reponame);
+          let labelString = "";
+          allLabels.data.forEach(label => {
+            labelString += label.name + ","
+          });
+          client.query("Insert into labels (RepoID, RepoName, Owner, Labels) values (" + BigInt(repoID) + ", '" + reponame + "', '" + owner + "', '" + labelString + "');");
+        }
+        else {
+          if(context.payload.action == 'edited' && !(typeof(context.payload.changes.name) === "undefined")) {
+            var labels = result.rows[0].labels.split(',');
+            var newLabelString = "";
+            for(i = 0; i < labels.length-1; i++) {
+              if(labels[i] == context.payload.changes.name.from) {
+                labels[i] = context.payload.label.name;
+              }
+              newLabelString += labels[i] + ',';
+            }
+            client.query("update labels set Labels = '" + newLabelString + "' where RepoID = " + BigInt(repoID) + ";")
+          }
+          else if(context.payload.action == 'deleted') {
+            var labels = result.rows[0].labels.split(',');
+            var newLabelString = "";
+            for(i = 0; i < labels.length-1; i++) {
+              if(labels[i] != context.payload.label.name) {
+                newLabelString += labels[i] + ',';
+              }
+            }
+            client.query("update labels set Labels = '" + newLabelString + "' where RepoID = " + BigInt(repoID) + ";")
+          }
+          else if(context.payload.action == 'created') {
+            var labels = result.rows[0].labels;
+            labels += context.payload.label.name + ',';
+            client.query("update labels set Labels = '" + labels + "' where RepoID = " + BigInt(repoID) + ";")
+          }
+        }
+      }
+    );
   });
+}
 
+//Creates the labels table if for some reason it's not around.
+async function createLabelsTable() {
+  await client.connect();
+  client.query("CREATE TABLE IF NOT EXISTS labels (RepoID bigint not null primary key, RepoName varchar(50) not null, Owner varchar(50) not null, Labels varchar(200))",
+                function(err, result) {
+                  if(err) { console.log("Could not create table"); };
+                }
+  );
+}
+
+async function getAllLabels(Owner, Repo) {
+  const octo = new Octokit();
+  return await octo.request('GET /repos/{owner}/{repo}/labels', {
+                  owner: Owner,
+                  repo: Repo
+               });
+}
   // For more information on building apps:
   // https://probot.github.io/docs/
 
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
-};
